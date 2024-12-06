bc8d0075e66548b50013d9d60e2aa90a1e94f82b
AnaClaraprisma <an4claracouto@gmail.com>
2024-12-06 09:37:09 -0300

    desafio iniciado

---
+++ frontend/js/validacaoDeCadastro.js
@@ -0,0 +1,5 @@
+const nome = document.getElementById("nome");
+const email = document.getElementById("email");
+const dataDeNascimento = document.getElementById("dn");
+const phone = document.getElementById("phone");
+
